95ced8ddb5e2c26c49c3c9763f251d08e3375281
Mario Franca <35295805+SergJohn@users.noreply.github.com>
2025-08-30 13:49:00 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-MyPluginModule::Engine.routes.draw do
+ReplyOnSolutionModule::Engine.routes.draw do
   get "/examples" => "examples#index"
   # define routes here
 end
 
-Discourse::Application.routes.draw { mount ::MyPluginModule::Engine, at: "my-plugin" }
+Discourse::Application.routes.draw { mount ::ReplyOnSolutionModule::Engine, at: "reply_on_solution" }
