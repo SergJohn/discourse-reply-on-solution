3b21494a4c31f3f665ef54aa6585dd904965ac50
Mario Franca <35295805+SergJohn@users.noreply.github.com>
2025-08-31 18:56:58 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-ReplyOnSolutionModule::Engine.routes.draw do
+DiscourseReplyOnSolutionModule::Engine.routes.draw do
   get "/examples" => "examples#index"
   # define routes here
 end
 
-Discourse::Application.routes.draw { mount ::ReplyOnSolutionModule::Engine, at: "reply_on_solution" }
+Discourse::Application.routes.draw { mount ::DiscourseReplyOnSolutionModule::Engine, at: "discourse_reply_on_solution" }
