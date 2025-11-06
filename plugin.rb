06b0863e8543d6e4d79a8ebeef8cbf1bac6aff91
Mario Franca <35295805+SergJohn@users.noreply.github.com>
2025-11-06 13:07:30 +0000

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-reply-on-solution
 # about: Replies to topics when a solution is accepted
-# version: 0.5
+# version: 0.6
 # authors: SergJohn
 
 enabled_site_setting :discourse_reply_on_solution_enabled
