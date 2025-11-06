5ad2b937aa72cefd21a06a8c701dcc29b2c8bbdd
Mario Franca <35295805+SergJohn@users.noreply.github.com>
2025-11-06 13:18:48 +0000

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -8,13 +8,14 @@
 enabled_site_setting :discourse_reply_on_solution_enabled
 
 after_initialize do
-  if defined?(DiscourseAutomation)
+  if defined?(DiscourseAutomation) && defined?(DiscourseSolved)
     add_automation_scriptable("discourse_reply_on_solution") do
       field :reply_text, component: :message
       
       version 1
       
-      triggerables [:first_accepted_solution] if defined?(DiscourseSolved)
+      triggerables [:first_accepted_solution]
+      
       script do |context, fields, automation|
         accepted_post_id = context["accepted_post_id"]
         accepted_post = Post.find_by(id: accepted_post_id)
@@ -32,6 +33,7 @@ after_initialize do
       
         # Only add reply if this topic does not already have it
         already_replied = Post.where(topic_id: topic.id).where("raw LIKE ?", "%#{marker}%").exists?
+        
         unless already_replied
           begin
             PostCreator.create!(
